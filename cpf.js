becc775efb55899f3ca1915becf29c68a00b7d7a
natieledpaula <natiele887@gmail.com>
2025-05-15 11:11:41 -0300

    Validar do 1º DV

--- cpf.js
+++ cpf.js
@@ -9,3 +9,17 @@ function validarCPF(cpf) {
 let soma = 0;
 let resto;
 
+// Validar do 1º DV
+for(let i= 1; i <= 9; i++) {
+    soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+}
+
+resto = (soma * 10) % 11;
+
+if(resto === 10 || resto === 11) {
+    resto = 0;
+}
+
+if(resto !== parseInt(cpf.substring(9, 10))) {
+    return false;
+}
