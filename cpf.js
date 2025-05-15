0788bed77d00a85896acc6fb400fdde74b622dbc
natieledpaula <natiele887@gmail.com>
2025-05-15 11:18:38 -0300

    Validar do 2º DV

--- cpf.js
+++ cpf.js
@@ -1,25 +1,45 @@
 function validarCPF(cpf) {
-    cpf = cpf.replace(/[^\d]+/g, "");
+        cpf = cpf.replace(/[^\d]+/g, "");
 
-    if(cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+        if(cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+            return false;
+        }
+
+
+    let soma = 0;
+    let resto;
+
+    // Validar do 1º DV
+    for(let i= 1; i <= 9; i++) {
+        soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+
+    if(resto === 10 || resto === 11) {
+        resto = 0;
+    }
+
+    if(resto !== parseInt(cpf.substring(9, 10))) {
         return false;
     }
-}
 
-let soma = 0;
-let resto;
+    // Validar do 2º DV
+    soma = 0;
 
-// Validar do 1º DV
-for(let i= 1; i <= 9; i++) {
-    soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
-}
+    for(let i = 1; i <= 10; i++) {
+        soma += parseInt(cpf.substring(i -1, i)) * (12 - i);
+    }
 
-resto = (soma * 10) % 11;
+    resto = (soma * 10) % 11;
 
-if(resto === 10 || resto === 11) {
-    resto = 0;
-}
+    if(resto === 10 || resto === 11) {
+        resto = 0;
+    }
 
-if(resto !== parseInt(cpf.substring(9, 10))) {
-    return false;
+    if(resto !== parseInt(cpf.substring(10, 11))) {
+        return false;
+    }
+
+    return true;
 }
